8da7e0ac5823a2d46714d42e77614e24c8b5ac7a
Romacardozx <roman.alejandro.cardozo@gmail.com>
2021-11-24 05:47:25 -0300

    Creacion de componente example para pruebas

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExampleComponent } from './example/example.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"", //TODO: local host:4200/
+    component:ExampleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
